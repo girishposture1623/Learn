a13012cc1d2d960dcacd341bd1a996a26a85c2e8
girishposture1623 <girishposture1623@gmail>
2026-06-19 17:05:43 +0530

    upload first

---
+++ basic/src/first.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const First = () => {
+  return (
+    <>
+      <div>
+        <h1>
+          Hello My Name Is <b>GIRISH</b>
+        </h1>
+      </div>
+      <div>
+        <h3>Learning React</h3>
+      </div>
+    </>
+  );
+};
+
+export default First;
